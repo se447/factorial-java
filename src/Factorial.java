d07b5aae5f1da54b0178be684439213176a4a531
Saad E <21321555+se447@users.noreply.github.com>
2019-03-19 21:03:39 -0400

    Calculate factorial of 0

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,8 +1,31 @@
+/*
+Create a program that calculates the factorial of the number n. 
+The factorial n! is calculated using the formula 1*2*3*...*n. 
+For example 4! = 1*2*3*4 = 24. Additionally, it is defined that 0! = 1.
+*/
+
 import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        
+        int factorial = 1;
+        
+        if (number == 0) {
+            factorial = 1;
+        }
+        
+        int i = 1;
+        while (i < number) {
+            factorial *= i * (i+1);
+            i++;
+        }
+        
+        
+        System.out.println("Factorial is " + factorial);
     }
 }
